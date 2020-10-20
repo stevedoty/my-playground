4bdd4dc94dcf34aa2d8b77b9bbe2442c9bf426b9
Steve Doty <stevedoty16@gmail.com>
2020-10-20 13:07:16 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <div className="App-Header">
+        <div className="App-Header_Navbar">
+          app navbar
+          <button className="App-Header_Navbar-Button">button</button>
+        </div>
+      </div>
+
+
+      <div className="App-Body">
+        <div className="App-Body_Todos">
+          <ul>
+            this is my todo-list
+            <li  className="App-Body_Todos-Item">first item</li>
+          </ul>
+        </div>
+        <div className="App-Body_Gallery">
+          <div className="App-Body_Gallery-Item">
+          </div>
+          <div className="App-Body_Gallery-Item">
+          </div>
+          <div className="App-Body_Gallery-Item">
+          </div>
+        </div>
+      </div>
+
+
+      <div className="App-Footer">
+        this is my Footer
+      </div>
+
+
     </div>
   );
 }
