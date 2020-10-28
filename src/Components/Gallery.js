fae4510c4a0fde122b33b458db32e171c1101012
Steve Doty <stevedoty16@gmail.com>
2020-10-28 14:45:27 -0700

    basic gallery layout

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import starscape from '../Assets/starscape.jpg';
+import springBlossoms from '../Assets/spring-blossoms.jpg';
+import summerClovers from '../Assets/summer-clovers.jpg';
+import autumnTrees from '../Assets/autumn-trees.jpg';
+import winterIce from '../Assets/winter-ice.jpg';
+
 
 class Gallery extends Component {
 
@@ -19,15 +24,69 @@ constructor(props){
         <div
           style={{
             position:"relative",
-            height:"400px", 
+            height:"400px",
             overflow: "hidden"}}>
         <img src={starscape}
           style={{
             position:"absolute",
-            transform: "translate(-50%,-50%) scale(0.4)",
+            transform: "translate(-50%,-50%) scale(0.3)",
             top:"50%",
             left:"50%"}}/>
         </div>
+
+        <div
+          style={{
+            position:"relative",
+            height:"400px",
+            overflow: "hidden"}}>
+        <img src={springBlossoms}
+          style={{
+            position:"absolute",
+            transform: "translate(-50%,-50%) scale(0.3)",
+            top:"50%",
+            left:"50%"}}/>
+        </div>
+
+        <div
+          style={{
+            position:"relative",
+            height:"400px"
+            }}>
+        <img src={summerClovers}
+          style={{
+            position:"absolute",
+            transform: "translate(-50%,-50%) scale(0.25)",
+            top:"50%",
+            left:"50%"}}/>
+        </div>
+
+        <div
+          style={{
+            position:"relative",
+            height:"400px",
+            overflow: "hidden"}}>
+        <img src={autumnTrees}
+          style={{
+            position:"absolute",
+            transform: "translate(-50%,-50%) scale(0.3)",
+            top:"50%",
+            left:"50%"}}/>
+        </div>
+
+        <div
+          style={{
+            position:"relative",
+            height:"400px",
+            overflow: "hidden"}}>
+        <img src={winterIce}
+          style={{
+            position:"absolute",
+            transform: "translate(-50%,-50%) scale(0.3)",
+            top:"50%",
+            left:"50%"}}/>
+        </div>
+
+
 
       </div>
     );
