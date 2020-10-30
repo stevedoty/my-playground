018bdbb5147eb224c64f6995b3447e24d3c891d5
Steve Doty <stevedoty16@gmail.com>
2020-10-30 14:14:44 -0700

    add css variables

--- src/Components/Messenger.js
+++ src/Components/Messenger.js
@@ -4,6 +4,15 @@ class Messenger extends Component {
   constructor(props){
     super(props);
     this.state = {
+      users : [
+        {
+          name:"Steve",
+          picture:"IMG",
+          number:["1234567890"],
+          email:["steve@emailsite.com"],
+          social:["steve@socialsite.com"]
+        }
+      ],
       messages : [
         "connect to server",
         "add timestamp",
@@ -43,13 +52,13 @@ class Messenger extends Component {
           <button className="App-Body_Messenger-Button"
             onClick={this.handleMessengerButtonClick}>sendMessage</button>
           <div className="Messenger_UserProfile">
-          <div className="User_Name">name</div>
-          <div className="User_Picture">picture</div>
-          <div className="User_Number">number</div>
-          <div className="User_Email">email</div>
-          <div className="User_Social">social</div>
+          <div className="User_Name">name:{this.state.users[0].name}</div>
+          <div className="User_Picture">picture:{this.state.users[0].picture}</div>
+          <div className="User_Number">number:{this.state.users[0].number[0]}</div>
+          <div className="User_Email">email:{this.state.users[0].email[0]}</div>
+          <div className="User_Social">social:{this.state.users[0].social[0]}</div>
           </div>
-          Conversation
+          <div className="Messenger_Conversation-Title" style={{textAlign:"left",marginLeft:"5em"}}>Conversation</div>
           <ul>
             {this.state.messages.map( element => (
               <div  className="App-Body_Messenger-Item">{element}</div>
