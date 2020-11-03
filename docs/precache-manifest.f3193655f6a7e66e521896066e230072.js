f71768641e3400c145f635c5b4b1b86ba0e7f5ae
Steve Doty <stevedoty16@gmail.com>
2020-11-03 09:00:58 -0800

    build /docs folder for github pages

---
+++ docs/precache-manifest.f3193655f6a7e66e521896066e230072.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9ed0ead26bbada61ea30c2dd0af65c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04efa865d5b8fe5a9e58",
+    "url": "/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "a0dd067d3b10dc2c72e1",
+    "url": "/static/css/main.94d50def.chunk.css"
+  },
+  {
+    "revision": "04efa865d5b8fe5a9e58",
+    "url": "/static/js/2.94e70c9d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.94e70c9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0dd067d3b10dc2c72e1",
+    "url": "/static/js/main.523a74cd.chunk.js"
+  },
+  {
+    "revision": "e976325c399f140de220",
+    "url": "/static/js/runtime-main.8072f1af.js"
+  },
+  {
+    "revision": "3e8afa311d4738c4cecc03fac81f8189",
+    "url": "/static/media/autumn-trees.3e8afa31.jpg"
+  },
+  {
+    "revision": "d5061335f549bbe953337d4350599b96",
+    "url": "/static/media/spring-blossoms.d5061335.jpg"
+  },
+  {
+    "revision": "feaccbaac24f94a23d2ba36d4efba5d9",
+    "url": "/static/media/starscape.feaccbaa.jpg"
+  },
+  {
+    "revision": "e4fb68c0fc479e2b190c3203a922ccea",
+    "url": "/static/media/summer-clovers.e4fb68c0.jpg"
+  },
+  {
+    "revision": "8b10c36d78fd405d9b5a3be19df0fe87",
+    "url": "/static/media/winter-ice.8b10c36d.jpg"
+  }
+]);
